ebaf01b30909c9725ebb9e2ffa645302074c4985
Subhanshu Prasad <103764915+subhanshu1111@users.noreply.github.com>
2023-05-29 11:16:24 +0545

    final update (resetting the game)

--- script.js
+++ script.js
@@ -10,21 +10,36 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
 //starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  scores = [0, 0]; //
+  currentScore = 0;
+  activePlayer = 0; //
+  playing = true; //
+
+  score0El.textContent = 0; //
+  score1El.textContent = 0; //
+  current0El.textContent = 0; //
+  current1El.textContent = 0; //
+
+  //diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 //switch player
+
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
-let activePlayer = 0;
-const scores = [0, 0];
-let currentScore = 0;
-let playing = true;
+
 //rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -57,7 +72,7 @@ btnHold.addEventListener('click', function () {
     //check if score is >=100
     //finish game
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -73,3 +88,4 @@ btnHold.addEventListener('click', function () {
 
   //switchPlayer
 });
+btnNew.addEventListener('click', init);
